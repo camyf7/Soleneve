1d0e1653283f095a7d2f3e6136b044b68151a8a6
camyf7 <camilly.ferreira@aluno.ifsp.edu.br>
2026-02-20 13:32:46 -0300

    Configuration for Netlify

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // SUAS CONFIGURAÇÕES ATUAIS
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-  
-  // NOVAS CONFIGURAÇÕES PARA RESOLVER OS ERROS
-  turbopack: false,  // DESATIVA O TURBOPACK
-  
-  allowedDevOrigins: ['localhost', '10.81.205.37', '192.168.*', '10.*']  // RESOLVE O CORS
+  output: 'standalone',
 }
 
-export default nextConfig
+module.exports = nextConfig
